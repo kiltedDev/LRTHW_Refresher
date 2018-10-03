eb53a6a739fb5f26cf7f5cf7e86e510e2b475e8d
Thomas Wilson <twilson.spqr@gmail.com>
2018-10-02 22:24:09 -0400

    very british in its flavor text, ex45 done

--- ex45.rb
+++ ex45.rb
@@ -1,19 +1,26 @@
-require 'pry'
+# require 'pry'
 
-class Player
+class Character
   def initialize ()
     @hit_points = 10
-    @alive = true
     @weapon = "fist"
     @max_damage = 4
   end
 
-  def life_check()
+  attr_accessor :weapon, :max_damage
+
+  def is_alive()
     if hit_points < 1
       return false
     else
       return true
     end
+  end
+
+  def equip(weapon, damage)
+    @weapon = weapon
+    @max_damage = damage
+    puts "Equipped #{weapon}"
   end
 end
 
@@ -27,25 +34,25 @@ class Engine
 
   def initialize (scene_map)
     @scene_map = scene_map
-    @player = Player.new()
   end
 
-  def play()
+  def play(player1)
     current_scene = @scene_map.opening_scene()
     last_scene = @scene_map.next_scene('finished')
+    player = player1
 
     while current_scene != last_scene
-      next_scene_name = current_scene.enter()
+      next_scene_name = current_scene.enter(player)
       current_scene = @scene_map.next_scene(next_scene_name)
     end
 
-    current_scene.enter()
+    current_scene.enter(player)
   end
 end
 
 class Death < Scene
 
-  def enter
+  def enter(player)
     deaths = [
       "Well, that was embarassing",
       "But you meant to do that right?",
@@ -59,7 +66,8 @@ class Death < Scene
 end
 
 class Glade < Scene
-  def enter()
+  def enter(player)
+    @player = player
     sword_in_stone = true
     still_here = true
 
@@ -74,30 +82,27 @@ class Glade < Scene
       end
       puts "What do you do?"
       print "> "
-      the_first_choice = $stdin.gets.chomp.downcase
+      glade_choice = $stdin.gets.chomp.downcase
 
-      if the_first_choice == "west"
+      if glade_choice == "west"
         puts "You brush some branches aside and head away from the clearing."
         return 'river'
-      elsif the_first_choice == "south"
+      elsif glade_choice == "south"
         puts "The trees thin as you leave the forest."
         return 'meadow'
-      elsif the_first_choice == "sword"
+      elsif glade_choice == "sword"
         luck_roll = rand(1..20)
         if luck_roll == 20
           puts "You brace both feet and lift with all your might."
           puts "You may not be the chosen one, but you've managed to lift the sword, stone and all."
           puts "You're pretty sure you could do some decent damage with this."
-          @@player.weapon = "Sword in the Stone"
-          @@player.max_damage = 12
+          player.equip("Sword in the Stone", 12)
           sword_in_stone = false
         elsif luck_roll > 1
           puts "You brace both feet and lift with all your might."
           puts "You are the king / queen / on-binary regent of Albion!"
           puts "Also, you have a sword with which you can defend yourself."
-          binding.pry
-          @player.weapon = "Magic Sword"
-          @player.max_damage = 10
+          player.equip("Magic Sword", 10)
           sword_in_stone = false
         else
           puts "You wish for a bottle of ibuprofen."
@@ -115,27 +120,100 @@ class Glade < Scene
 end
 
 class River < Scene
-  def enter()
+  def enter(player)
+    @player = player
+    puts "Hey, look, a River"
+    puts "WATERFALL or POOL"
+    print "> "
+    river_choice = $stdin.gets.chomp.downcase
+
+    if river_choice == "waterfall"
+      puts "You brush some branches aside and head away from the clearing."
+      return 'waterfall'
+    elsif river_choice == "pool"
+      puts "The trees thin as you leave the forest."
+      return 'pool'
+    end
   end
 end
 
 class Waterfall < Scene
-  def enter()
+  def enter(player)
+    @player = player
+    puts "Hey, look, a waterfall"
+    puts "CAVE or RIVER"
+    print "> "
+    waterfall_choice = $stdin.gets.chomp.downcase
+
+    if waterfall_choice == "cave"
+      puts "You go to the cave"
+      return 'death'
+    elsif waterfall_choice == "river"
+      puts "You go to the river."
+      return 'river'
+    end
+  end
+end
+
+class Pool < Scene
+  def enter(player)
+    @player = player
+    puts "Hey, look, a forest pool"
+    puts "RUN or OTHER"
+    print "> "
+    pool_choice = $stdin.gets.chomp.downcase
+
+    if pool_choice == "run"
+      puts "You go to the river"
+      return 'river'
+    else
+      puts "You go to the death."
+      return 'death'
+    end
   end
 end
 
 class Meadow < Scene
-  def enter()
+  def enter(player)
+    @player = player
+    puts "Hey, look, a meadow, and sheep"
+    puts "SHEEP or COTTAGE"
+    print "> "
+    meadow_choice = $stdin.gets.chomp.downcase
+
+    if meadow_choice == "sheep"
+      puts "You go to the fight a wolf"
+      return 'death'
+    elsif meadow_choice == "cottage"
+      puts "You go to the cottage."
+      return 'cottage'
+    end
   end
 end
 
 class Cottage < Scene
-  def enter()
+  def enter(player)
+    @player = player
+    puts "Hey, look, a dinner table"
+    puts "FATHER or DAUGHTER"
+    print "> "
+    cottage_choice = $stdin.gets.chomp.downcase
+
+    if cottage_choice == "father"
+      puts "You go to the father"
+      puts "He eats you"
+      return 'death'
+    elsif cottage_choice == "daughter"
+      puts "You go to the daughter."
+      puts "She eats you"
+      return 'death'
+    end
   end
 end
 
 class Finished < Scene
-  def enter()
+  def enter(player)
+    @player = player
     puts "You won! Good job."
   end
 end
@@ -145,6 +223,7 @@ class Map
       'glade' => Glade.new(),
       'river' => River.new(),
       'waterfall' => Waterfall.new(),
+      'pool' => Pool.new(),
       'meadow' => Meadow.new(),
       'cottage' => Cottage.new(),
       'death' => Death.new(),
@@ -165,6 +244,7 @@ class Map
   end
 end
 
+player = Character.new()
 a_map = Map.new('glade')
 a_game = Engine.new(a_map)
-a_game.play()
+a_game.play(player)
